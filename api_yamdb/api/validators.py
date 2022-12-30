fd9d24b13d011b6cb3a6ad842a7bd2c56e9bc503
Andrey Danilov <kekmaster69@yandex.ru>
2022-12-30 19:15:37 +0200

    reworked username and email validation

--- api_yamdb/api/validators.py
+++ api_yamdb/api/validators.py
@@ -1,19 +1,26 @@
 from rest_framework.exceptions import ValidationError
 from users.models import User
+import re
+
+username_pattern = re.compile(r'^[\w.@+-]+\Z')
 
 
 def validate_username(value):
     if value == 'me':
         raise ValidationError('Недопустимое имя пользователя')
-    elif User.objects.filter(username=value).exists():
+    if not username_pattern.match(value):
+        raise ValidationError(
+            'Cringe username try again'
+        )
+
+
+def validate_username_or_email_exists(username, email):
+    if User.objects.filter(username=username).exists():
         raise ValidationError(
             'Пользователь c таким именем '
             'уже зарегестрирован'
         )
-
-
-def validate_email(value):
-    if User.objects.filter(email=value).exists():
+    if User.objects.filter(email=email).exists():
         raise ValidationError(
             'Пользователь с такой почтой '
             'уже зарегестрирован'
